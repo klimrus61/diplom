61e69e4a1c979155b0acac0098e1cc4660ce8fab
klimrus61 <klimrus61@gmail.com>
2022-05-29 16:45:27 +0300

    templates https://djangocentral.com/building-a-blog-application-with-django/

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',), }
+
+
 admin.site.register(models.Worker)
 admin.site.register(models.Moderator)
+admin.site.register(models.Article, ArticleAdmin)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-from django.template.defaultfilters import slugify # new
+from django.template.defaultfilters import slugify  # new
 from django.urls import reverse
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
 
 class Moderator(models.Model):
@@ -27,21 +33,26 @@ class Worker(models.Model):
     def get_absolute_url(self):
         return reverse('worker_detail', kwargs={'slug': self.slug})
 
-    def save(self, *args, **kwargs): # new auto slug outside admin.py
+    def save(self, *args, **kwargs):  # new auto slug outside admin.py
         if not self.slug:
             self.slug = slugify(self.full_name)
         return super().save(*args, **kwargs)
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=50)
-    period = models.DurationField()
-    trips_num = models.DecimalField(max_digits=2, decimal_places=0)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
+    created_on = models.DateField(auto_now=True, editable=True)
+    text_b = models.TextField(blank=True, null=True, help_text='Введите номера билетов')
+    trips_num = models.DecimalField(max_digits=2, decimal_places=0)  #Кол во поездок
     num_days_worked = models.DecimalField(max_digits=2, decimal_places=0, blank=True, null=True)
-    day_verify = models.DateField(blank=True, null=True) # day checks
+    day_verify = models.DateField(blank=True, null=True)  # day checks
     author = models.ForeignKey(Worker, on_delete=models.CASCADE)
     pers_check = models.ForeignKey(Moderator, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-
